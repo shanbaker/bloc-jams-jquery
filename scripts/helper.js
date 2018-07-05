8cb35cfbc7a16718a4ac7cf9b78cb8ddd6e72edd
shanbaker <36170850+shanbaker@users.noreply.github.com>
2018-07-05 02:59:14 -0400

    added helper.js

---
+++ scripts/helper.js
@@ -0,0 +1,11 @@
+class Helper {
+  playPauseAndUpdate(song) {
+    player.playPause(song);
+
+    const duration = player.currentlyPlaying.duration;
+    $('#time-control .total-time').text(duration);
+
+  }
+}
+
+const helper = new Helper();
