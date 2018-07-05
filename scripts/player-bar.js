b55d24fb20c2a60f427eaf99b83ceef36ee4043b
Shan Baker <shanbakmusic@gmail.com>
2018-07-05 00:39:18 -0400

    Added helper.js

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -25,4 +25,25 @@
         const previousSong = album.songs[previousSongIndex];
         player.playPause(previousSong);
     });
+
+    $('#time-control input').on('input', function (event) {
+        player.skipTo(event.target.value);
+    });
+
+    setInterval( () => {
+        if (player.playState !== 'playing') { return; }
+        const currentTime = player.getTime();
+        const duration = player.getDuration();
+        const percent = (currentTime / duration) * 100;
+        $('#time-control .current-time').text( currentTime )
+        $('#time-control input').val(percent);
+    }, 1000);
+
+    $('#volume-control input').on('input', function (event) {
+        player.setVolume(event.target.value);
+    });
+
+
+    
+        
 }
